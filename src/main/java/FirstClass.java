f832d4f1ecfcfaaa50d4df4a89a4d928a3ad1e45
Dawid Malecki <dmalecki997@gmail.com>
2022-09-14 14:39:51 +0200

    git push

--- src/main/java/FirstClass.java
+++ src/main/java/FirstClass.java
@@ -1,5 +1,23 @@
-public class FirstClass {                           // [1]
-    public static void main(String[] args) {         // [2]
-        System.out.println("Hello from FirstClass!"); // [3]
+public class FirstClass {
+    public static void main(String[] args) {
+
+        Notebook notebook = new Notebook(600, 1000, 2015);
+        System.out.println(notebook.weight + " " + notebook.price + " " + notebook.year + " " );
+        notebook.checkPrice();
+        notebook.checkWeight();
+        notebook.discountConditions();
+
+        Notebook heavyNotebook = new Notebook(2000, 1500, 2018);
+        System.out.println(heavyNotebook.weight + " " + heavyNotebook.price + " " + notebook.year);
+        heavyNotebook.checkPrice();
+        heavyNotebook.checkWeight();
+        heavyNotebook.discountConditions();
+
+        Notebook oldNotebook = new Notebook(1600, 500, 2020);
+        System.out.println(oldNotebook.weight + " " + oldNotebook.price + " " + notebook.year);
+        oldNotebook.checkPrice();
+        oldNotebook.checkWeight();
+        heavyNotebook.discountConditions();
+
     }
 }
