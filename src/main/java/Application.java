9142ea7ca0d4b8b5d9c01562d39a2d0fb3fdf2ec
Dawid Malecki <dmalecki997@gmail.com>
2022-09-21 10:40:07 +0200

    git push

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -2,23 +2,68 @@ class Application {
     public static void main(String[] args) throws Exception {
         People Adam = new People("Adam", 40.5, 178);
 
-        boolean validateName = true;
-        boolean validateAge = true;
-        boolean validateHeight = true;
+        boolean validateName;
+        boolean validateAge;
+        boolean validateHeight;
 
-        boolean notValidateName = false;
-        boolean notValidateAge = false;
-        boolean notValidateHeight = false;
-
-        if(Adam.age > 30 && Adam.height > 160){
-            System.out.println(validateAge);
-            System.out.println(validateHeight);
-            System.out.println(validateName);
-        } else if(Adam.age < 30 && Adam.height < 160) {
-            System.out.println(notValidateAge);
-            System.out.println(notValidateHeight);
-            System.out.println(notValidateName);
+        if (Adam.age > 30 && Adam.height > 160) {
+            validateName = true;
+            validateAge = true;
+            validateHeight = true;
+        } else {
+            validateAge = false;
+            validateHeight = false;
+            validateName = false;
         }
+        System.out.println(validateAge);
+        System.out.println(validateHeight);
+        System.out.println(validateName);
+
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+       // if(Adam.age > 30 && Adam.height > 160){
+            //System.out.println(validateAge);
+            //System.out.println(validateHeight);
+            //System.out.println(validateName);
+       // } else if(Adam.age < 30 && Adam.height < 160) {
+            //System.out.println(notValidateAge);
+            //System.out.println(notValidateHeight);
+            //System.out.println(notValidateName);
+
+
+
 
