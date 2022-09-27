f956788867f28bc49405a2e226c8439114170ae4
Dawid Malecki <dmalecki997@gmail.com>
2022-09-27 16:38:50 +0200

    git push

--- kodilla-basic-tests/src/main/java/com/kodilla/abstracts/homework/Person.java
+++ kodilla-basic-tests/src/main/java/com/kodilla/abstracts/homework/Person.java
@@ -1,36 +1,36 @@
 package com.kodilla.abstracts.homework;
 
+
+
 public class Person {
     public static void main(String[] args) {
 
-      int driverAge = 32;
-      int ButcherAge =34;
+      int ageDriver = 32;
+      int ageButcher = 34;
       String Rzeznik = "Rzeźnik";
       String Driver = "Kierowca";
-      String driverName = "Andrzej";
-      String butcherName = "Piotr";
+      String driverFirstName = "Andrzej";
+      String butcherFirstName = "Piotr";
+      Job job = new Butcher();
+      Job job1 = new Driver();
 
 
-
-
-
-
-
-        Driver myDriver = new Driver();
-
-        Butcher myButcher = new Butcher();
-     myDriver.test();
-
-
-        myButcher.test();
 
 
         PersonProcessor processor = new PersonProcessor();
-        processor.process(myDriver);
-        System.out.println(Driver+ ":"+driverName);
+        processor.process(job);
+        System.out.println(butcherFirstName+":lat:"+ageButcher+":"+Rzeznik+"\n");
+        processor.process(job1);
+        System.out.println(driverFirstName+":lat:"+ageDriver+":"+Driver+"\n");
 
-        processor.process(myButcher);
-        System.out.println(Rzeznik+":"+butcherName);
+
+
+
+
+
+
+
+
 
 
 
