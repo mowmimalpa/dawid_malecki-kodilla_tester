3fd81f57fcf605f40bbc8ac1837c7c9e3074cf52
Dawid Malecki <dmalecki997@gmail.com>
2022-10-24 12:44:38 +0200

    git push

--- kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/GamblingMachineTest.java
+++ kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/GamblingMachineTest.java
@@ -14,18 +14,44 @@ class GamblingMachineTest {
 
     GamblingMachine gamblingMachine = new GamblingMachine();
 
+    //wyjatek
     @ParameterizedTest
     @CsvFileSource(resources = "/numbersToFail.csv")
-    public void shouldThrownExceptionWithIncorrectNumbers(String numbers){
-        String [] expectedArray = numbers.split(",");
+    public void shouldThrownExceptionWithIncorrectNumbers(String numbers) {
+        String[] expectedArray = numbers.split(",");
         Set<String> expectedSet = new HashSet<>(Arrays.asList(expectedArray));
-        List<Integer>numbersToCheck = expectedSet
+        List<Integer> numbersToCheck = expectedSet
                 .stream()
                 .map(u -> Integer.parseInt(u))
                 .collect(Collectors.toList());
-        Set<Integer>expectedParse = new HashSet<>(numbersToCheck);
-        Assertions.assertThrows(InvalidNumbersException.class,()-> gamblingMachine.validateNumbers(expectedParse));
+        Set<Integer> expectedParse = new HashSet<>(numbersToCheck);
+        Assertions.assertThrows(InvalidNumbersException.class, () -> gamblingMachine.validateNumbers(expectedParse));
     }
 
+    @ParameterizedTest
+    @CsvFileSource(resources = "/numbersToFail.csv")
+    public void shouldNotCountWithIncorrectNumbers(String numbers) throws InvalidNumbersException {
+        String[] expectedArray = numbers.split(",");
+        Set<String> expectedSet = new HashSet<>(Arrays.asList(expectedArray));
+        List<Integer> numbersToCheck = expectedSet
+                .stream()
+                .map(u -> Integer.parseInt(u))
+                .collect(Collectors.toList());
+        Set<Integer> expectedParse = new HashSet<>(numbersToCheck);
+        Assertions.assertThrows(InvalidNumbersException.class, () -> gamblingMachine.howManyWins(expectedParse));
 
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/numbersToPass.csv")
+    public void shouldPassWithCorrectNumbers(String numbers) throws InvalidNumbersException {
+        String[] expectedArray = numbers.split(",");
+        Set<String> expectedSet = new HashSet<>(Arrays.asList(expectedArray));
+        List<Integer> numbersToCheck = expectedSet
+                .stream()
+                .map(u -> Integer.parseInt(u))
+                .collect(Collectors.toList());
+        Set<Integer> expectedParse = new HashSet<>(numbersToCheck);
+
+    }
 }
