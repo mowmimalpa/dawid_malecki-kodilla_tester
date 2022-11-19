89be5a2f78a9df375f7708c4f08560cd55ef0792
Dawid Malecki <dmalecki997@gmail.com>
2022-11-19 16:30:17 +0100

    git push

--- kodilla-rest-api/src/test/java/com/kodilla/rest/controller/BookControllerTest.java
+++ kodilla-rest-api/src/test/java/com/kodilla/rest/controller/BookControllerTest.java
@@ -2,14 +2,11 @@ package com.kodilla.rest.controller;
 
 import com.kodilla.rest.domain.BookDto;
 import com.kodilla.rest.service.BookService;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
-
 import java.util.ArrayList;
-import java.util.List;
-
 import static org.assertj.core.api.Assertions.assertThat;
-
 class BookControllerTest {
 
     @Test
@@ -35,13 +32,14 @@ class BookControllerTest {
         BookService bookServiceMock = Mockito.mock(BookService.class);
         BookController bookController = new BookController(bookServiceMock);
         List<BookDto> booksList = new ArrayList<>();
-        //when
-        BookDto book = bookController.addBook(new BookDto("Title 3", "Author 3"));
-        booksList.add(book);
-        System.out.println(booksList.size());
+        booksList.add(new BookDto("Title 1","Author 1"));
+        booksList.add(new BookDto("Title 2","Author 2"));
         Mockito.when(bookServiceMock.getBooks()).thenReturn(booksList);
+        //when
+        bookController.addBook(new BookDto("Title 1","Author 1"));
+        bookController.addBook(new BookDto("Title 2","Author 2"));
         //then
-        assertThat(booksList).hasSize(1);
-
+        Mockito.verify(bookServiceMock).addBook(new BookDto("Title 1", "Author 1"));
+        Mockito.verify(bookServiceMock).addBook(new BookDto("Title 2", "Author 2"));
     }
 }
