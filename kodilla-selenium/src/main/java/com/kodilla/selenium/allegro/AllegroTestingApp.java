1f47b66447232d5c1e68158c70d9e3206ccd9a7c
Dawid Malecki <dmalecki997@gmail.com>
2022-12-06 16:58:11 +0100

    git push

---
+++ kodilla-selenium/src/main/java/com/kodilla/selenium/allegro/AllegroTestingApp.java
@@ -0,0 +1,42 @@
+package com.kodilla.selenium.allegro;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
+
+
+public class AllegroTestingApp {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "c:\\selenium-drivers\\chrome\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.ebay.com");
+
+
+        WebElement category = driver.findElement(By.cssSelector("#gh-cat"));
+        Select type = new Select(category);
+        type = new Select(category);
+        type.selectByIndex(6);
+
+        WebElement itemName = driver.findElement(By.cssSelector("#gh-ac"));
+        itemName.sendKeys("mini mavic");
+
+        WebElement clickButton = driver.findElement(By.cssSelector("#gh-btn"));
+        clickButton.click();
+
+        Thread.sleep(2000);
+
+
+        List<WebElement> elementFound = driver.findElements(By.cssSelector("#srp-river-results > ul > li"));
+        for (WebElement element : elementFound) {
+            System.out.println(element.getText());
+        }
+    }
+}
