6bb9f94e26c6bae9756d5e83a255c7b6d6a7d4bb
Dawid Malecki <dmalecki997@gmail.com>
2022-12-29 18:50:55 +0100

    git push

--- kodilla-code-coverage/src/test/java/com/kodilla/jdbc/DbManagerTestSuite.java
+++ kodilla-code-coverage/src/test/java/com/kodilla/jdbc/DbManagerTestSuite.java
@@ -1,48 +1,91 @@
-//package com.kodilla.jdbc;
-//
-//import java.sql.ResultSet;
-//import java.sql.Statement;
-//import java.util.AbstractMap;
-//import java.util.List;
-//import org.junit.jupiter.api.Assertions;
-//import org.junit.jupiter.api.Test;
-//import org.junit.jupiter.api.BeforeAll;
-//import java.sql.SQLException;
-//
-//class DbManagerTestSuite {
-//    private static DbManager dbManager;
-//
-//    @BeforeAll
-//    public static void setup() throws SQLException {
-//        dbManager = DbManager.getInstance();
-//    }
-//
-//    @Test
-//    void testConnect() {
-//        //Given
-//        //When
-//        //Then
-//        Assertions.assertNotNull(dbManager.getConnection());
-//    }
-//
-//    @Test
-//    void testSelectUsers() throws SQLException {
-//        //Given
-//        //When
-//        String sqlQuery = "SELECT * FROM USERS";
-//        Statement statement = dbManager.getConnection().createStatement();
-//        ResultSet rs = statement.executeQuery(sqlQuery);
-//
-//        //Then
-//        int counter = 0;
-//        while(rs.next()) {
-//            System.out.println(rs.getInt("ID") + ", " +
-//                    rs.getString("FIRSTNAME") + ", " +
-//                    rs.getString("LASTNAME"));
-//            counter++;
-//        }
-//        rs.close();
-//        statement.close();
-//        Assertions.assertEquals(5, counter);
-//    }
-//}
+package com.kodilla.jdbc;
+
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.AbstractMap;
+import java.util.List;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeAll;
+import java.sql.SQLException;
+
+ class DbManagerTestSuite {
+    @Test
+    void testConnect() throws SQLException {
+        //Given
+        //When
+        DbManager dbManager = DbManager.getInstance();
+        //Then
+        Assertions.assertNotNull(dbManager.getConnection());
+    }
+
+    @Test
+     void testUsersSelect() throws SQLException {
+        //Given
+        DbManager dbManager = DbManager.getInstance();
+        String countQuery = "SELECT COUNT(*) FROM USERS";
+        Statement statement = dbManager.getConnection().createStatement();
+        ResultSet rs = statement.executeQuery(countQuery);
+        int count = 0;
+        while (rs.next()) {
+            count = rs.getInt("COUNT(*)");
+        }
+        String sql = "INSERT INTO USERS(FIRSTNAME, LASTNAME) VALUES ('Zara', 'Ali')";
+        statement.executeUpdate(sql);
+        sql = "INSERT INTO USERS(FIRSTNAME, LASTNAME) VALUES ('Otman', 'Use')";
+        statement.executeUpdate(sql);
+        sql = "INSERT INTO USERS(FIRSTNAME, LASTNAME) VALUES ('Mark', 'Boq')";
+        statement.executeUpdate(sql);
+        sql = "INSERT INTO USERS(FIRSTNAME, LASTNAME) VALUES ('Uli', 'Wimer')";
+        statement.executeUpdate(sql);
+        sql = "INSERT INTO USERS(FIRSTNAME, LASTNAME) VALUES ('Oli', 'Kosiw')";
+        statement.executeUpdate(sql);
+        //When
+        String sqlQuery = "SELECT * FROM USERS";
+        statement = dbManager.getConnection().createStatement();
+        rs = statement.executeQuery(sqlQuery);
+        //Then
+        int counter = 0;
+        while (rs.next()) {
+            System.out.println(rs.getInt("ID") + ", " +
+                    rs.getString("FIRSTNAME") + ", " +
+                    rs.getString("LASTNAME"));
+            counter++;
+        }
+        int expected = count + 5;
+        rs.close();
+        statement.close();
+        Assertions.assertEquals(expected, counter);
+
+    }
+
+    @Test
+     void testSelectUsersAndPosts() throws SQLException {
+        // given
+        DbManager dbManager = DbManager.getInstance();
+        String countQuery = "SELECT  COUNT(*) FROM USERS U JOIN POSTS P ON U.ID = P.USER_ID GROUP BY P.USER_ID HAVING COUNT(*) = 2";
+        Statement statement = dbManager.getConnection().createStatement();
+        ResultSet rs = statement.executeQuery(countQuery);
+        int count = 0;
+        while (rs.next()) {
+            count = rs.getInt("COUNT(*)");
+        }
+        //When
+        String sqlQuery = "SELECT U.FIRSTNAME, U.LASTNAME, COUNT(*) FROM USERS U JOIN POSTS P ON U.ID = P.USER_ID GROUP BY P.USER_ID HAVING COUNT(*) = 2";
+        statement = dbManager.getConnection().createStatement();
+        rs = statement.executeQuery(sqlQuery);
+        //Then
+        int counter = 0;
+        while (rs.next()) {
+            System.out.println(rs.getString("FIRSTNAME") + ", " +
+                    rs.getString("LASTNAME"));
+            counter++;
+        }
+        int expected = count;
+        rs.close();
+        statement.close();
+        Assertions.assertEquals(expected, counter);
+    }
+}
+
+
